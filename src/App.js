4be9202c0441f26b62c9f80e0ed5abc8f2632709
sirloinus <amaliesmidth@gmail.com>
2019-06-03 16:45:08 +0100

    created basic store and greeting reducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createStore } from 'redux';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
 export default App;
+
+
+const defaultState = {
+  greeting: 'hi',
+  otherState: 'some stuff',
+}
+
+const greetingReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'GREET_ME':
+      return { ...state, greeting: 'hi amalie'};
+    case 'GREET_WORLD':
+      return { ...state, greeting: 'hello world'};
+    default:
+      return state;
+  }
+};
+
+const store = createStore(greetingReducer);
+
+store.dispatch({
+  type: 'GREET_ME'
+})
+
+console.log(store.getState())
